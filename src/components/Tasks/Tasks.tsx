ca63622f536e9e6823f08085e875fd54faf5288d
Susanna Lakey <susannalakey@gmail.com>
2021-02-04 17:37:12 -0800

    implemented filtering of tasks based on active, completed, or all tasks

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -5,22 +5,26 @@ import Box from '@material-ui/core/Box';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import sun from '../../img/sun.png';
 import moon from '../../img/moon.png';
 import { Task } from '../../types';
 import './Tasks.css';
-import { getTasks, updateTaskStatus, deleteTask } from '../../api/todos';
+import { getAll, updateStatus, deleteTask } from '../../api/todos';
+
+type Filter = 'All' | 'Active' | 'Completed';
 
 const Tasks = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [isloading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [filter, setFilter] = useState<Filter>('All');
 
   const fetchData = async () => {
     setIsError(false);
     setIsLoading(true);
     try {
-      const data = await getTasks();
+      const data = await getAll();
       setTasks(data);
     } catch (error) {
       setIsError(true);
@@ -38,7 +42,7 @@ const Tasks = () => {
     const updatedTasksList = tasks.map((task: Task) =>
       task.id === data.id ? { ...task, completed: newCompleted } : task
     );
-    updateTaskStatus(data);
+    updateStatus(data);
     setTasks(updatedTasksList);
   };
 
@@ -51,10 +55,25 @@ const Tasks = () => {
   const getCompletedClass = (completed: boolean) =>
     completed ? 'completed' : 'incompleted';
 
-  const calculateTotalCompleted = (tasks: Task[]): number => {
+  const calculateTotalLeft = (tasks: Task[]): string => {
     const completed = tasks.filter(task => task.completed === true);
-    return completed.length;
+    const totalCompleted = completed.length;
+    const totalLeft = tasks.length - totalCompleted;
+    return totalLeft > 1 || totalLeft === 0
+      ? `${totalLeft} items left`
+      : `${totalLeft} item left`;
   };
+
+  const completedTasks = tasks.filter(task => task.completed === true);
+  const activeTasks = tasks.filter(task => task.completed === false);
+
+  const filterToTasks: Record<Filter, Task[]> = {
+    All: tasks,
+    Active: activeTasks,
+    Completed: completedTasks,
+  };
+
+  const tasksToDisplay = filterToTasks[filter];
 
   const setAvatarImage = (completed: boolean) => (completed ? moon : sun);
   return (
@@ -65,50 +84,96 @@ const Tasks = () => {
         {isloading ? (
           <CircularProgress />
         ) : (
-          <Box>
-            {tasks.map((task: any) => (
-              <Box
-                borderBottom={1}
-                borderColor='error.main'
-                width={400}
-                mb={4}
-                key={task.id}
-                display='flex'
-                flexDirection='row'
-                justifyContent='space-between'
+          <>
+            <Box ml={5}>
+              <ButtonGroup
+                color='primary'
+                variant='text'
+                size='small'
+                aria-label='small outlined button group'
               >
-                <Box display='flex' flexDirection='row'>
-                  <Avatar
-                    alt={`${setAvatarImage(task.completed)}`}
-                    src={setAvatarImage(task.completed)}
-                    style={{
-                      width: '30px',
-                      height: '30px',
-                      marginRight: '10px',
-                    }}
-                  />
-                  <Typography
-                    className={getCompletedClass(task.completed)}
-                    variant='h5'
-                    onClick={() => updateCompleted(task)}
-                  >
-                    {task.text}
-                  </Typography>
+                <Button
+                  className={filter === 'All' ? 'active' : ''}
+                  onClick={() => {
+                    setFilter('All');
+                  }}
+                >
+                  All
+                </Button>
+
+                <Button
+                  className={filter === 'Active' ? 'active' : ''}
+                  onClick={() => {
+                    setFilter('Active');
+                  }}
+                >
+                  Active
+                </Button>
+                <Button
+                  className={filter === 'Completed' ? 'active' : ''}
+                  onClick={() => {
+                    setFilter('Completed');
+                  }}
+                >
+                  Completed
+                </Button>
+              </ButtonGroup>
+            </Box>
+            <Box>
+              {tasksToDisplay.map((task: any) => (
+                <Box
+                  borderBottom={1}
+                  borderColor='error.main'
+                  width={400}
+                  mb={4}
+                  key={task.id}
+                  display='flex'
+                  flexDirection='row'
+                  justifyContent='space-between'
+                >
+                  <Box display='flex' flexDirection='row'>
+                    <Avatar
+                      alt={`${setAvatarImage(task.completed)}`}
+                      src={setAvatarImage(task.completed)}
+                      style={{
+                        width: '30px',
+                        height: '30px',
+                        marginRight: '10px',
+                      }}
+                    />
+                    <Typography
+                      className={getCompletedClass(task.completed)}
+                      variant='h5'
+                      onClick={() => updateCompleted(task)}
+                    >
+                      {task.text}
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Button
+                      color='secondary'
+                      size='large'
+                      onClick={() => removeTask(task)}
+                    >
+                      x
+                    </Button>
+                  </Box>
                 </Box>
-                <Box>
-                  <Button
-                    color='secondary'
-                    size='large'
-                    onClick={() => removeTask(task)}
-                  >
-                    X
-                  </Button>
-                </Box>
-              </Box>
-            ))}
-          </Box>
+              ))}
+            </Box>
+          </>
         )}
-        {calculateTotalCompleted(tasks)}
+
+        <Box display='flex' flexDirection='row'>
+          <Box
+            mr={4}
+            style={{
+              fontSize: '18px',
+            }}
+          >
+            {calculateTotalLeft(tasks)}
+          </Box>
+        </Box>
       </Box>
     </>
   );
